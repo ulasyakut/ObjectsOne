dfb7761aeb4487d8d60a4bff324e45631b7ee3d2
Ulas Yakut <uyakut@Zipcoders-MacBook-Pro-5.local>
2024-10-24 07:42:30 -0400

    second update

---
+++ object1/src/main/java/object1/Car.java
@@ -0,0 +1,47 @@
+package object1;
+
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+    private int mileage;
+
+    public Car(String make, String model, int year, int mileage) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+}
